2818989c06524e303cc11db01e88b4ce4b57ec37
Darien Martinez Torres <darien.martinez@gmail.com>
2025-06-29 18:44:32 -0400

    Location key

--- src/pages/LocationTab.js
+++ src/pages/LocationTab.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-const GOOGLE_MAPS_API_KEY = "AIzaSyAv80U70-3sZ1aaJMHE672gp_cLfdqMRNE";
-
 const LocationTab = () => (
   <div id="tab-panel-location" role="tabpanel">
     <div className="rounded-lg shadow bg-white p-8 text-center">
@@ -9,7 +7,7 @@ const LocationTab = () => (
       <p className="text-gray-700 mb-2">Darien operates from Burlington, Ontario, Canada.</p>
       <div className="flex justify-center my-6">
         <img
-          src={`https://maps.googleapis.com/maps/api/staticmap?center=Burlington,ON,Canada&zoom=12&size=400x256&markers=color:red%7Clabel:B%7C43.3256,-79.7990&key=${GOOGLE_MAPS_API_KEY}`}
+          src={`https://maps.googleapis.com/maps/api/staticmap?center=Burlington,ON,Canada&zoom=12&size=400x256&markers=color:red%7Clabel:B%7C43.3256,-79.7990&key=AIzaSyAv80U70-3sZ1aaJMHE672gp_cLfdqMRNE`}
           alt="Map of Burlington, Ontario, Canada"
           className="w-full max-w-md h-64 border-2 border-blue-200 rounded-lg shadow object-cover"
         />
